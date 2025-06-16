e34c52f129689e4a126ded1a418db0495e8db697
Azabraza322 <kartman_345@mail.ru>
2025-06-16 12:27:00 +0500

    подключение railway

--- backend/server/server.ts
+++ backend/server/server.ts
@@ -20,11 +20,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // ===== 1. Подключение к MySQL =====
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
