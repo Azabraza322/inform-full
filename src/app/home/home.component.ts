60a07e19a8be810f05ccdebd7ca903058255398a
Azabraza322 <kartman_345@mail.ru>
2025-06-18 04:29:45 +0500

    оптимизация и адаптация под телефон

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { RouterModule } from '@angular/router'; // ✅ Обязательно добавить
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,25 +13,25 @@ export class HomeComponent implements OnInit, OnDestroy {
   products = [
     {
       title: 'Контур.ОФД',
-      description: 'Онлайн-передача фискальных данных...',
+      description: 'Онлайн-передача фискальных данных в ФНС согласно требованиям 54-ФЗ. Отправка чеков по СМС и e-mail. Аналитика по торговым точкам.',
       icon: 'fa-file-contract',
       color: 'white'
     },
     {
       title: 'Контур.Зарплата',
-      description: 'Расчет заработной платы...',
+      description: 'Легкий и понятный расчет заработной платы и налоговых отчислений. Подготовка необходимой отчетности в ФНС, ФСС, ПФР и пр.',
       icon: 'fa-wallet',
       color: 'white'
     },
     {
       title: 'Контур.Маркет',
-      description: 'Автоматизация бизнеса...',
+      description: 'Товарный учет в режиме онлайн. Касса и печать номенклатуры в чеке в соответствии с 54-ФЗ. Работа с ЕГАИС.',
       icon: 'fa-shopping-cart',
       color: 'white'
     },
     {
       title: 'Контур.Персонал',
-      description: 'Автоматизация кадрового делопроизводства...',
+      description: 'Автоматизируйте все этапы кадрового учета. Отраслевые решения: Госслужба, Медицина, Производство.',
       icon: 'fa-users',
       color: 'white'
     }
